673f5eb7331088aff53ea8bcb145f5a4c0b9fcbd
AndresEspositoDe <98119676+AndresEspositoDe@users.noreply.github.com>
2024-03-09 13:00:45 -0400

    ✨Comenzando Login, Conectando Firebase🗃️

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARCwZPX9zQV6B-GQmL72cbrlm8PTkmNRg",
+  authDomain: "eustaquiogames-cd17b.firebaseapp.com",
+  projectId: "eustaquiogames-cd17b",
+  storageBucket: "eustaquiogames-cd17b.appspot.com",
+  messagingSenderId: "1063701166979",
+  appId: "1:1063701166979:web:28222aaa2e171b1ada94b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
